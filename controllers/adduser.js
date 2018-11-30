73e22b59270c59f6def59f6796b7f01c6a76cc60
zhangyao <zhang_yao@uxsino.com>
2018-11-30 18:18:06 +0800

    添加sql

--- controllers/adduser.js
+++ controllers/adduser.js
@@ -3,33 +3,33 @@ const Data = require('./../common/common');
 const md5 = require('md5'); // 密码加密;
 const createId = require('./../common/createstr'); //随机生成32位id
 exports.add = async(ctx, next) => {
-        // let query = () => {
-        console.log(createId())
-            // return new Promise((reject, reslove) => {
-            //     db.query("SELECT * FROM `add_user`", (err, data) => {
-            //         let req = ctx.request.body;
-            //         let pass = md5(ctx.request.body.pass);
-            //         let id = createId();
-            //         //加密密码
-            //         if (!err) {
-            //             console.log(data, req);
-            //             if (data.length <= 0) {
-            //                 //加密 用户密码
-            //                 db.query("INSERT INTO add_user (user,pass,id) VALUES (req.user,pass,id)", (err, res) => {
-            //                     if (!err) {
-            //                         console.log(err)
-            //                     } else {
-            //                         console.log(res);
-            //                     }
-            //                 })
-            //             } else {
+    let query = () => {
+        let req = ctx.request.body;
+        let password = md5(ctx.request.body.pass);
+        let id = createId();
+        return new Promise((reject, reslove) => {
+            console.log(req, password, id)
+            db.query(`SELECT * FROM add_user`, (err, data) => {
+                //加密密码
+                if (!err) {
+                    console.log(data, req);
+                    if (data.length <= 0) {
+                        //加密 用户密码
+                        db.query(`INSERT INTO add_user (user, pass_word, id) VALUES ('${req.user}', '${password}', '${id}');`, (err, res) => {
+                            if (!err) {
+                                console.log(res);
+                            } else {
+                                console.log(err);
+                            }
+                        })
+                    } else {
 
-        //             }
-        //         } else {
-        //             reject(err);
-        //         }
-        //     })
-        // })
+                    }
+                } else {
+                    reject(err);
+                }
+            })
+        })
     }
-    // let result = await query();
+    let result = await query();
 }
