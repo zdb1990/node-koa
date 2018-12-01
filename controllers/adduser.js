12935b428d360f9a281e7ff9ea6dd0f96bb026ec
zhangyao <zhangyao@parim.net>
2018-12-01 20:25:43 +0800

    注册接口

--- controllers/adduser.js
+++ controllers/adduser.js
@@ -2,34 +2,46 @@ const db = require('./../config/createpool');
 const Data = require('./../common/common');
 const md5 = require('md5'); // 密码加密;
 const createId = require('./../common/createstr'); //随机生成32位id
-exports.add = async(ctx, next) => {
-    let query = () => {
-        let req = ctx.request.body;
-        let password = md5(ctx.request.body.pass);
-        let id = createId();
-        return new Promise((reject, reslove) => {
-            console.log(req, password, id)
-            db.query(`SELECT * FROM add_user`, (err, data) => {
-                //加密密码
-                if (!err) {
-                    console.log(data, req);
-                    if (data.length <= 0) {
-                        //加密 用户密码
-                        db.query(`INSERT INTO add_user (user, pass_word, id) VALUES ('${req.user}', '${password}', '${id}');`, (err, res) => {
-                            if (!err) {
-                                console.log(res);
-                            } else {
-                                console.log(err);
-                            }
-                        })
-                    } else {
-
-                    }
-                } else {
+//查找用户
+const select=(username)=>{
+    return new Promise((reslove,reject)=>{
+        console.log(`${username}`);
+        db.query(`SELECT * FROM add_user WHERE user='${username}' `,(err,data)=>{
+            if(!err){
+                let resData=Data.handelData(data);
+                reslove(resData);
+            }else{
+               reject(err);
+            }
+        })
+    })
+}
+//注册用户
+exports.add=async(ctx,next)=>{
+    let user={
+        name:ctx.request.body.user,
+        pass:md5(ctx.request.body.pass),
+        id:createId(),
+    }
+    let dbres=await select(user.name);
+    if(dbres.length>0){
+       ctx.status=200;
+       ctx.body={
+           msg:'user exist',
+           success:false
+       }
+    }else{
+        await new Promise((reslove,reject)=>{
+            console.log(1);
+            db.query(`INSERT INTO add_user (user, pass, id) VALUES ('${user.name}', '${user.pass}', '${user.id}');`,(err,data)=>{
+                if(!err){
+                    reslove()
+                }else{
                     reject(err);
                 }
-            })
+           });
         })
-    }
-    let result = await query();
+        ctx.status = 200;
+        ctx.body={success:true,status:500,msg:'注册成功'};
+    };
 }
